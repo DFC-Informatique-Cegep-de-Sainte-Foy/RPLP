47d4ee4c9bfbdd9ec0c7d2b6d9149693cce72e6e
Olena Zagorna <1992473@csfoy.ca>
2020-06-09 20:36:22 -0400

    Correction dans le procedure d'ajoute de professeur dans la BD Olena

--- SQL/revueParLesPairs_script.sql
+++ SQL/revueParLesPairs_script.sql
@@ -5,19 +5,31 @@ use revupaire;
 -- ------------------------------------------------------------
 drop procedure if exists ajouterProfesseur;
 delimiter $$
-create procedure ajouterProfesseure(in p_nom varchar(45), p_prenom varchar(45), p_email varchar(45))
+create procedure ajouterProfesseur(in p_nom varchar(45), p_prenom varchar(45), p_email varchar(45))
 Begin
-if(p_nom is null) 
-then
-	select 'Fin de la procedure. Le nom ne doit pas etre null!' as Erreur;
-elseif (p_email is null)
-then
-	select 'Fin de la procedure. E-mail ne doit pas etre null!' as Erreur;
-else 
-	insert into professeur(nom,prenom,email) 
-		value (p_nom,p_prenom,p_email);
-	select * from professeur;
-end if;
+-- -------------------------------------
+-- VARIABLES LOCALES
+-- -------------------------------------
+	declare v_nbProfesseur int default 0;
+-- -------------------------------------
+-- VALIDATIONS
+-- -------------------------------------
+	if(p_nom is null) then
+		select 'Fin de la procedure. Le nom ne doit pas etre null!' as Erreur;
+	elseif (p_email is null) then
+		select 'Fin de la procedure. E-mail ne doit pas etre null!' as Erreur;
+	else 
+		select count(*) into v_nbProfesseur from professeur 	
+			where (nom = p_nom && prenom = p_prenom && email = p_email);
+		if (v_nbProfesseur > 0) 
+        then
+			select 'Fin de la procedure. Le professeur est deja dans la base de donnee!' as Erreur;
+        else
+			insert into professeur(nom,prenom,email) 
+				value (p_nom,p_prenom,p_email);
+			select * from professeur;
+		end if;
+	end if;
 End $$
 delimiter ;
 
