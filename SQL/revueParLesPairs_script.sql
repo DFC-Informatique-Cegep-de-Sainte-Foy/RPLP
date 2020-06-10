f28fcefa4f6745f261fe06e0208fd0d3c8d15c2c
Olena Zagorna <1992473@csfoy.ca>
2020-06-09 20:40:01 -0400

    Creation procedure pour ajouter Admin dans la BD Olena

--- SQL/revueParLesPairs_script.sql
+++ SQL/revueParLesPairs_script.sql
@@ -20,16 +20,57 @@ Begin
 		select 'Fin de la procedure. E-mail ne doit pas etre null!' as Erreur;
 	else 
 		select count(*) into v_nbProfesseur from professeur 	
-			where (nom = p_nom && prenom = p_prenom && email = p_email);
-		if (v_nbProfesseur > 0) 
+		where (nom = p_nom && prenom = p_prenom && email = p_email);
+		
+        if (v_nbProfesseur > 0) 
         then
 			select 'Fin de la procedure. Le professeur est deja dans la base de donnee!' as Erreur;
         else
 			insert into professeur(nom,prenom,email) 
-				value (p_nom,p_prenom,p_email);
+			value (p_nom,p_prenom,p_email);
 			select * from professeur;
 		end if;
 	end if;
 End $$
 delimiter ;
 
+-- ------------------------------------------------------------
+-- PROCEDURE POUR AJOUTER L'ADMIN
+-- @auteur Olena Zagorna
+-- ------------------------------------------------------------
+drop procedure if exists ajouterAdmin;
+delimiter $$
+create procedure ajouterAdmin(in p_nom varchar(45), p_prenom varchar(45), p_email varchar(45))
+Begin
+-- -------------------------------------
+-- VARIABLES LOCALES
+-- -------------------------------------
+	declare v_idAdmin int default null;
+-- -------------------------------------
+-- VALIDATIONS
+-- -------------------------------------
+	if(p_nom is null) 
+	then
+		select 'Fin de la procedure. Le nom ne doit pas etre null!' as Erreur;
+	elseif (p_email is null)
+	then
+		select 'Fin de la procedure. E-mail ne doit pas etre null!' as Erreur;
+	else 
+		select idProfesseur into v_idAdmin from professeur 	
+		where (nom = p_nom & prenom = p_prenom & email = p_email);
+		
+        if (v_idAdmin is null) then
+			insert into professeur(nom,prenom,email) 
+			value (p_nom,p_prenom,p_email);
+			
+			select idProfesseur into v_idAdmin from professeur 
+			order by idProfesseur DESC limit 1;
+		   
+			update professeur set isAdmin = 1 where idProfesseur like v_idAdmin;
+		else
+			update professeur set isAdmin = 1 where idProfesseur like v_idAdmin;
+		end if;
+		select * from professeur;
+	end if;
+End $$
+delimiter ;
