5d308f54b45489ff360aea18ff7deef7a864bfdb
Pierre-Francois Léon <pierrefrancois.leon@gmail.com>
2020-08-21 18:42:48 +0000

    Updated Program.cs

--- RplpAvecBD/Program.cs
+++ RplpAvecBD/Program.cs
@@ -13,6 +13,10 @@ namespace RplpAvecBD
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddEnvironmentVariables();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
