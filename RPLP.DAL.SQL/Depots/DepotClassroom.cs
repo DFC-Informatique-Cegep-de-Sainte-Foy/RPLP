8744b35f1cedd8e04607b9e116eab9b98e01fe5e
Jonathan Blouin <blouinjonathan07@gmail.com>
2022-03-15 14:35:19 -0400

    Fix depot Classroom

--- RPLP.DAL.SQL/Depots/DepotClassroom.cs
+++ RPLP.DAL.SQL/Depots/DepotClassroom.cs
@@ -35,11 +35,44 @@ namespace RPLP.DAL.SQL.Depots
 
         public void UpsertClassroom(Classroom p_classroom)
         {
-            Classroom classroomResult = this._context.Classrooms.Where(classroom => classroom.Id == p_classroom.Id).Select(classroom => classroom.ToEntity()).FirstOrDefault();
+            List<Student_SQLDTO> students = new List<Student_SQLDTO>();
+            List<Teacher_SQLDTO> teachers = new List<Teacher_SQLDTO>();
+            List<Assignment_SQLDTO> assignments = new List<Assignment_SQLDTO>();
+
+            if (p_classroom.Students.Count >= 1)
+            {
+                foreach (Student student in p_classroom.Students)
+                {
+                    students.Add(new Student_SQLDTO(student));
+                }
+            }
+
+            if (p_classroom.Teachers.Count >= 1)
+            {
+                foreach (Teacher teacher in p_classroom.Teachers)
+                {
+                    teachers.Add(new Teacher_SQLDTO(teacher));
+                }
+            }
+
+            if (p_classroom.Assignment.Count >= 1)
+            {
+                foreach (Assignment assignment in p_classroom.Assignment)
+                {
+                    assignments.Add(new Assignment_SQLDTO(assignment));
+                }
+            }
+
+            Classroom_SQLDTO classroomResult = this._context.Classrooms.Where(classroom => classroom.Id == p_classroom.Id).FirstOrDefault();
 
             if (classroomResult != null)
             {
                 classroomResult.Name = p_classroom.Name;
+                classroomResult.OrganisationName = p_classroom.OrganisationName;
+                classroomResult.Students = students;
+                classroomResult.Teachers = teachers;
+                classroomResult.Assignment = assignments;
+
                 this._context.Update(classroomResult);
                 this._context.SaveChanges();
             }
@@ -47,6 +80,10 @@ namespace RPLP.DAL.SQL.Depots
             {
                 Classroom_SQLDTO classDTO = new Classroom_SQLDTO();
                 classDTO.Name = p_classroom.Name;
+                classDTO.OrganisationName = p_classroom.OrganisationName;
+                classDTO.Students = students;
+                classDTO.Teachers = teachers;
+                classDTO.Assignment = assignments;
 
                 this._context.Classrooms.Add(classDTO);
                 this._context.SaveChanges();
