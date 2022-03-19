ef16209e74f86004b897357f6675367976b55bd9
Jonathan Blouin <blouinjonathan07@gmail.com>
2022-03-19 14:04:28 -0400

    Ajout methodes depot classroom

--- RPLP.DAL.SQL/Depots/DepotClassroom.cs
+++ RPLP.DAL.SQL/Depots/DepotClassroom.cs
@@ -1,4 +1,5 @@
-﻿using RPLP.DAL.DTO.Sql;
+﻿using Microsoft.EntityFrameworkCore;
+using RPLP.DAL.DTO.Sql;
 using RPLP.ENTITES;
 using RPLP.SERVICES.InterfacesDepots;
 using System;
@@ -18,13 +19,119 @@ namespace RPLP.DAL.SQL.Depots
             this._context = new RPLPDbContext();
         }
 
+        public void AddAssignmentToClassroom(string p_classroomName, Assignment p_assignment)
+        {
+            Classroom_SQLDTO classroomResult = this._context.Classrooms.Where(classroom => classroom.Name == p_classroomName)
+                                                                       .Include(classroom => classroom.Assignment)
+                                                                       .FirstOrDefault();
+            if (classroomResult != null)
+            {
+                Assignment_SQLDTO assignmentResult = this._context.Assignments.Where(assignment => assignment.Active)
+                                                                              .SingleOrDefault(assignment => assignment.Name == p_assignment.Name);
+
+                if (assignmentResult != null && !classroomResult.Assignment.Contains(assignmentResult))
+                {
+                    classroomResult.Assignment.Add(assignmentResult);
+
+                    this._context.Update(classroomResult);
+                    this._context.SaveChanges();
+                }
+            }
+        }
+
+        public void AddStudentToClassroom(string p_classroomName, Student p_student)
+        {
+            Classroom_SQLDTO classroomResult = this._context.Classrooms.Where(classroom => classroom.Name == p_classroomName)
+                                                           .Include(classroom => classroom.Students)
+                                                           .FirstOrDefault();
+            if (classroomResult != null)
+            {
+                Student_SQLDTO studentResult = this._context.Students.Where(student => student.Active)
+                                                                     .SingleOrDefault(student => student.Username == p_student.Username);
+
+                if (studentResult != null && !classroomResult.Students.Contains(studentResult))
+                {
+                    classroomResult.Students.Add(studentResult);
+
+                    this._context.Update(classroomResult);
+                    this._context.SaveChanges();
+                }
+            }
+        }
+
+        public void AddTeacherToClassroom(string p_classroomName, Teacher p_teacher)
+        {
+            Classroom_SQLDTO classroomResult = this._context.Classrooms.Where(classroom => classroom.Name == p_classroomName)
+                                                            .Include(classroom => classroom.Teachers)
+                                                            .FirstOrDefault();
+            if (classroomResult != null)
+            {
+                Teacher_SQLDTO teacherResult = this._context.Teachers.Where(student => student.Active)
+                                                                     .SingleOrDefault(student => student.Username == p_teacher.Username);
+
+                if (teacherResult != null && !classroomResult.Teachers.Contains(teacherResult))
+                {
+                    classroomResult.Teachers.Add(teacherResult);
+
+                    this._context.Update(classroomResult);
+                    this._context.SaveChanges();
+                }
+            }
+        }
+
+        public void DeleteClassroom(string p_classroomName)
+        {
+            Classroom_SQLDTO classroomResult = this._context.Classrooms.SingleOrDefault(classroom => classroom.Name == p_classroomName);
+
+            if (classroomResult != null)
+            {
+                classroomResult.Active = false;
+
+                this._context.Update(classroomResult);
+                this._context.SaveChanges();
+            }
+        }
+
+        public List<Assignment> GetAssignmentsByClassroomName(string p_classroomName)
+        {
+            Classroom_SQLDTO classroomResult = this._context.Classrooms.Where(classroom => classroom.Name == p_classroomName)
+                                                                       .Include(classroom => classroom.Assignment)
+                                                                       .FirstOrDefault();
+
+            if (classroomResult != null && classroomResult.Assignment.Count >= 1)
+                return classroomResult.Assignment.Select(assignment => assignment.ToEntity()).ToList();
+
+            return new List<Assignment>();
+        }
+
         public Classroom GetClassroomById(int p_id)
         {
-            Classroom classroom = this._context.Classrooms.Where(classroom => classroom.Id == p_id).Select(classroom => classroom.ToEntity()).FirstOrDefault();
-
-            if (classroom == null)
-            {
+            Classroom_SQLDTO classroomResult = this._context.Classrooms.Where(classroom => classroom.Id == p_id)
+                                                                       .Include(classroom => classroom.Teachers)
+                                                                       .Include(classroom => classroom.Students)
+                                                                       .Include(classroom => classroom.Assignment)
+                                                                       .FirstOrDefault();
+            if (classroomResult == null)
                 return new Classroom();
+
+            Classroom classroom = classroomResult.ToEntityWithoutList();
+
+            if (classroomResult.Students.Count >= 1)
+            {
+                List<Student> students = classroomResult.Students.Select(student => student.ToEntityWithoutList()).ToList();
+                classroom.Students = students;
+            }
+
+            if (classroomResult.Teachers.Count >= 1)
+            {
+                List<Teacher> teachers = classroomResult.Teachers.Select(teacher => teacher.ToEntityWithoutList()).ToList();
+                classroom.Teachers = teachers;
+            }
+
+            if (classroomResult.Assignment.Count >= 1)
+            {
+                List<Assignment> assignments = classroomResult.Assignment.Select(student => student.ToEntity()).ToList();
+                classroom.Assignment = assignments;
             }
 
             return classroom;
@@ -32,17 +139,151 @@ namespace RPLP.DAL.SQL.Depots
 
         public Classroom GetClassroomByName(string p_name)
         {
-            Classroom classroom = this._context.Classrooms.Where(classroom => classroom.Name == p_name).Select(classroom => classroom.ToEntity()).FirstOrDefault();
-
-            if (classroom == null)
+            Classroom_SQLDTO classroomResult = this._context.Classrooms.Where(classroom => classroom.Name == p_name)
+                                                                       .Include(classroom => classroom.Teachers)
+                                                                       .Include(classroom => classroom.Students)
+                                                                       .Include(classroom => classroom.Assignment)
+                                                                       .FirstOrDefault();
+            if (classroomResult == null)
                 return new Classroom();
+
+            Classroom classroom = classroomResult.ToEntityWithoutList();
+
+            if (classroomResult.Students.Count >= 1)
+            {
+                List<Student> students = classroomResult.Students.Select(student => student.ToEntityWithoutList()).ToList();
+                classroom.Students = students;
+            }
+
+            if (classroomResult.Teachers.Count >= 1)
+            {
+                List<Teacher> teachers = classroomResult.Teachers.Select(teacher => teacher.ToEntityWithoutList()).ToList();
+                classroom.Teachers = teachers;
+            }
+
+            if (classroomResult.Assignment.Count >= 1)
+            {
+                List<Assignment> assignments = classroomResult.Assignment.Select(student => student.ToEntity()).ToList();
+                classroom.Assignment = assignments;
+            }
 
             return classroom;
         }
 
         public List<Classroom> GetClassrooms()
         {
-            return this._context.Classrooms.Select(classroom => classroom.ToEntity()).ToList();
+            List<Classroom_SQLDTO> classesResult = this._context.Classrooms.Where(classroom => classroom.Active)
+                                                                       .Include(classroom => classroom.Teachers)
+                                                                       .Include(classroom => classroom.Students)
+                                                                       .Include(classroom => classroom.Assignment)
+                                                                       .ToList();
+            if (classesResult.Count <= 0)
+                return new List<Classroom>();
+            else
+            {
+                List<Classroom> classes = classesResult.Select(classroom => classroom.ToEntityWithoutList()).ToList();
+
+                for (int i = 0; i < classesResult.Count; i++)
+                {
+                    if (classesResult[i].Id == classes[i].Id)
+                    {
+                        if (classesResult[i].Students.Count >= 1)
+                        {
+                            List<Student> students = classesResult[i].Students.Select(student => student.ToEntityWithoutList()).ToList();
+                            classes[i].Students = students;
+                        }
+
+                        if (classesResult[i].Teachers.Count >= 1)
+                        {
+                            List<Teacher> teachers = classesResult[i].Teachers.Select(teacher => teacher.ToEntityWithoutList()).ToList();
+                            classes[i].Teachers = teachers;
+                        }
+
+                        if (classesResult[i].Assignment.Count >= 1)
+                        {
+                            List<Assignment> assignments = classesResult[i].Assignment.Select(student => student.ToEntity()).ToList();
+                            classes[i].Assignment = assignments;
+                        }
+                    }
+                }
+
+                return classes;
+            }
+        }
+
+        public List<Student> GetStudentsByClassroomName(string p_classroomName)
+        {
+            Classroom_SQLDTO classroomResult = this._context.Classrooms.Where(classroom => classroom.Name == p_classroomName)
+                                                                       .Include(classroom => classroom.Assignment)
+                                                                       .FirstOrDefault();
+
+            if (classroomResult != null && classroomResult.Students.Count >= 1)
+                return classroomResult.Students.Select(assignment => assignment.ToEntityWithoutList()).ToList();
+
+            return new List<Student>();
+        }
+
+        public List<Teacher> GetTeachersByClassroomName(string p_classroomName)
+        {
+            Classroom_SQLDTO classroomResult = this._context.Classrooms.Where(classroom => classroom.Name == p_classroomName)
+                                                                       .Include(classroom => classroom.Teachers)
+                                                                       .FirstOrDefault();
+
+            if (classroomResult != null && classroomResult.Teachers.Count >= 1)
+                return classroomResult.Teachers.Select(teacher => teacher.ToEntityWithoutList()).ToList();
+
+            return new List<Teacher>();
+        }
+
+        public void RemoveAssignmentFromClassroom(string p_classroomName, int p_id)
+        {
+            Classroom_SQLDTO classroomResult = this._context.Classrooms.Where(classroom => classroom.Name == p_classroomName)
+                                                                       .Include(classroom => classroom.Assignment)
+                                                                       .FirstOrDefault();
+
+            Assignment_SQLDTO assignmentResult = this._context.Assignments.SingleOrDefault(assignment => assignment.Id == p_id);
+
+            if (classroomResult.Assignment.Contains(assignmentResult))
+            {
+                classroomResult.Assignment.Remove(assignmentResult);
+
+                this._context.Update(classroomResult);
+                this._context.SaveChanges();
+            }
+        }
+
+        public void RemoveStudentFromClassroom(string p_classroomName, string p_username)
+        {
+            Classroom_SQLDTO classroomResult = this._context.Classrooms.Where(classroom => classroom.Name == p_classroomName)
+                                                                       .Include(classroom => classroom.Students)
+                                                                       .FirstOrDefault();
+
+            Student_SQLDTO studentResult = this._context.Students.SingleOrDefault(student => student.Username == p_username);
+
+            if (classroomResult.Students.Contains(studentResult))
+            {
+                classroomResult.Students.Remove(studentResult);
+
+                this._context.Update(classroomResult);
+                this._context.SaveChanges();
+            }
+        }
+
+        public void RemoveTeacherFromClassroom(string p_classroomName, string p_username)
+        {
+            Classroom_SQLDTO classroomResult = this._context.Classrooms.Where(classroom => classroom.Name == p_classroomName)
+                                                                       .Include(classroom => classroom.Teachers)
+                                                                       .FirstOrDefault();
+
+            Teacher_SQLDTO teacherResult = this._context.Teachers.SingleOrDefault(teacher => teacher.Username == p_username);
+
+            if (classroomResult.Teachers.Contains(teacherResult))
+            {
+                classroomResult.Teachers.Remove(teacherResult);
+
+                this._context.Update(classroomResult);
+                this._context.SaveChanges();
+            }
         }
 
         public void UpsertClassroom(Classroom p_classroom)
--- RPLP.SERVICES/InterfacesDepots/IDepotClassroom.cs
+++ RPLP.SERVICES/InterfacesDepots/IDepotClassroom.cs
@@ -10,8 +10,18 @@ namespace RPLP.SERVICES.InterfacesDepots
     public interface IDepotClassroom
     {
         public List<Classroom> GetClassrooms();
-        public Classroom GetClassroomById(int id);
-        public Classroom GetClassroomByName(string name);
+        public Classroom GetClassroomById(int p_id);
+        public Classroom GetClassroomByName(string p_classroomName);
+        public List<Student> GetStudentsByClassroomName(string p_classroomName);
+        public List<Teacher> GetTeachersByClassroomName(string p_classroomName);
+        public List<Assignment> GetAssignmentsByClassroomName(string p_classroomName);
         public void UpsertClassroom(Classroom classroom);
+        public void AddTeacherToClassroom(string p_classroomName, Teacher p_teacher);
+        public void RemoveTeacherFromClassroom(string p_classroomName, string p_username);
+        public void AddStudentToClassroom(string p_classroomName, Student p_student);
+        public void RemoveStudentFromClassroom(string p_classroomName, string p_username);
+        public void AddAssignmentToClassroom(string p_classroomName, Assignment p_assignment);
+        public void RemoveAssignmentFromClassroom(string p_classroomName, int p_id);
+        public void DeleteClassroom(string p_classroomName);
     }
 }
