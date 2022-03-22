44a6e11f312c70f77542505fe809751c5f5d8d40
Jonathan Blouin <blouinjonathan07@gmail.com>
2022-03-22 16:04:12 -0400

    Retirer action controller github

--- RPLP.API/Controllers/GithubController.cs
+++ RPLP.API/Controllers/GithubController.cs
@@ -12,55 +12,7 @@ namespace RPLP.API.Controllers
 
         public GithubController()
         {
-            _githubAction = new GithubApiAction("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");
-        }
-
-        [HttpGet("{organisationName}")]
-        public ActionResult<IEnumerable<Repository_JSONDTO>> GetRepositories(string organisationName)
-        {
-            return Ok(this._githubAction.GetOrganisationRepositoriesGithub(organisationName));
-        }
-
-        [HttpGet("{organisationName}/{repositoryName}")]
-        public ActionResult<IEnumerable<Repository_JSONDTO>> GetRepository(string organisationName, string repositoryName)
-        {
-            return Ok(this._githubAction.GetRepositoryInfoGithub(organisationName, repositoryName));
-        }
-
-        [HttpGet("{organisationName}/{repositoryName}/Commit/Ref")]
-        public ActionResult<IEnumerable<Repository_JSONDTO>> GetRepositoryCommitRef(string organisationName, string repositoryName)
-        {
-            return Ok(this._githubAction.GetRepositoryCommitRefGithub(organisationName, repositoryName));
-        }
-
-        [HttpGet("{organisationName}/{repositoryName}/Commit/")]
-        public ActionResult<IEnumerable<Repository_JSONDTO>> GetRepositoryCommits(string organisationName, string repositoryName)
-        {
-            return Ok(this._githubAction.GetRepositoryCommitsGithub(organisationName, repositoryName));
-        }
-
-        [HttpGet("{organisationName}/{repositoryName}/BRanches/")]
-        public ActionResult<IEnumerable<Repository_JSONDTO>> GetRepositoryBranches(string organisationName, string repositoryName)
-        {
-            return Ok(this._githubAction.GetRepositoryBranchesGithub(organisationName, repositoryName));
-        }
-
-        [HttpPost("{organisationName}/{repositoryName}/Create/Branch/{branchName}/From/{sha}")]
-        public ActionResult<string> CreateNewBranch(string organisationName, string repositoryName, string branchName, string sha)
-        {
-            return Ok(this._githubAction.CreateNewBranchForFeedbackGitHub(organisationName, repositoryName, sha, branchName));
-        }
-
-        [HttpPost("{organisationName}/{repositoryName}/Create/PullRequest/{branchName}/Title/{title}/Body/{body}")]
-        public ActionResult<string> CreateNewPR(string organisationName, string repositoryName, string branchName, string title, string body)
-        {
-            return Ok(this._githubAction.CreateNewPullRequestFeedbackGitHub(organisationName, repositoryName, branchName, title, body));
-        }
-
-        [HttpPost("{organisationName}/{repositoryName}/Assign/Branch/{branchName}/Student/{studentUsername}")]
-        public ActionResult<string> AssignStidentToPR(string organisationName, string repositoryName, string branchName, string studentUsername)
-        {
-            return Ok(this._githubAction.AssignReviewerToPullRequestGitHub(organisationName, repositoryName, branchName, studentUsername));
+            //_githubAction = new GithubApiAction("");
         }
 
     }
