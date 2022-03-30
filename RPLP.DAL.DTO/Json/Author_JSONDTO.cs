47a794dc4d7dc654fbe269f72a93e4da6e129e57
Jonathan Blouin <blouinjonathan07@gmail.com>
2022-03-30 16:11:06 -0400

    retirer champ inutile

--- RPLP.DAL.DTO/Json/Author_JSONDTO.cs
+++ RPLP.DAL.DTO/Json/Author_JSONDTO.cs
@@ -9,7 +9,6 @@ namespace RPLP.DAL.DTO.Json
     public class Author_JSONDTO
     {
         public string name { get; set; }
-        public string email { get; set; }
         public DateTime date { get; set; }
     }
 }
