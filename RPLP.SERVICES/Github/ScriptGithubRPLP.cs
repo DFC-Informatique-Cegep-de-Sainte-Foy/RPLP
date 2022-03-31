4d3dc55deaa2d2c0bd3e2cbbc691deac16afd716
Jonathan Blouin <blouinjonathan07@gmail.com>
2022-03-31 13:00:12 -0400

    Amelioration script assignation

--- RPLP.SERVICES/Github/ScriptGithubRPLP.cs
+++ RPLP.SERVICES/Github/ScriptGithubRPLP.cs
@@ -23,97 +23,143 @@ namespace RPLP.SERVICES.Github
             this._githubApiAction = new GithubApiAction(p_token);
         }
 
-        public string ScriptAssignStudentToAssignmentReview(string p_organisationName, string p_classRoomName, string p_assignmentName, int p_ReviewsPerRepository)
+        public string ScriptAssignStudentToAssignmentReview(string p_organisationName, string p_classRoomName, string p_assignmentName, int p_reviewsPerRepository)
         {
-            if (p_ReviewsPerRepository <= 0 || string.IsNullOrWhiteSpace(p_organisationName) || string.IsNullOrWhiteSpace(p_classRoomName) || string.IsNullOrWhiteSpace(p_assignmentName))
+            if (p_reviewsPerRepository <= 0 || string.IsNullOrWhiteSpace(p_organisationName) || string.IsNullOrWhiteSpace(p_classRoomName) || string.IsNullOrWhiteSpace(p_assignmentName))
                 throw new ArgumentException("One of the provided value is incorrect or null");
 
+            string result = "";
             List<Student> students = _depotClassroom.GetStudentsByClassroomName(p_classRoomName);
 
-            if (students.Count >= p_ReviewsPerRepository + 1)
+            if (students.Count < p_reviewsPerRepository + 1)
             {
-                List<Repository> repositoriesToAssign = getRepositoriesToAssign(p_organisationName, p_classRoomName, p_assignmentName, students);
+                return "Number of studentss inferior to number of reviews";
+            }
 
-                foreach (Repository repository in repositoriesToAssign)
+            List<Repository> repositoriesToAssign = getRepositoriesToAssign(p_organisationName, p_classRoomName, p_assignmentName, students);
+            Dictionary<string, int> studentDictionary = new Dictionary<string, int>();
+
+            //Populer le dictionnaire d'assignation
+            foreach (Repository repository in repositoriesToAssign)
+            {
+                string[] splitRepository = repository.Name.Split('-');
+                string studentUsername = splitRepository[1];
+
+                studentDictionary[studentUsername] = 0;
+            }
+
+            //Faire l'action
+            foreach (Repository repository in repositoriesToAssign)
+            {
+                result = prepareRepositoryAndCreatePullRequest(p_organisationName, repository.Name, studentDictionary, p_reviewsPerRepository);
+            }
+
+            return result;
+        }
+
+        private string prepareRepositoryAndCreatePullRequest(string p_organisationName, string p_repositoryName, Dictionary<string, int> p_studentDictionary, int p_reviewsPerRepository)
+        {
+            string result = "";
+            string[] splitRepository = p_repositoryName.Split('-');
+            Branch_JSONDTO branchDTO = new Branch_JSONDTO();
+
+            List<Branch_JSONDTO> branchesResult = this._githubApiAction.GetRepositoryBranchesGithub(p_organisationName, p_repositoryName);
+
+            if (branchesResult == null)
+            {
+                throw new ArgumentNullException("Branch is null");
+            }
+
+            foreach (Branch_JSONDTO branch in branchesResult)
+            {
+                string[] branchName = branch.reference.Split("/");
+
+                if (branchName[2] == "feedback")
                 {
-                    string[] splitRepository = repository.Name.Split('-');
-                    Branch_JSONDTO branchDTO = new Branch_JSONDTO();
-                    List<Branch_JSONDTO> branchesResult = this._githubApiAction.GetRepositoryBranchesGithub(p_organisationName, repository.Name);
+                    branchDTO = branch;
+                    break;
+                }
+            }
 
-                    foreach (Branch_JSONDTO branch in branchesResult)
+            int numberStudentAdded = 0;
+
+            do
+            {
+                string username = p_studentDictionary.Where(dictionary => dictionary.Key.ToLower() != splitRepository[1].ToLower())
+                                                     .FirstOrDefault(dictionary => dictionary.Value == p_studentDictionary.Values.Min()).Key;
+
+                result = createPullRequestAndAssignUser(p_organisationName, p_repositoryName, branchDTO.gitObject.sha, username);
+                if (result != "Created")
+                    return result;
+
+                p_studentDictionary[username] = p_studentDictionary[username]++;
+                numberStudentAdded++;
+
+            } while (numberStudentAdded < p_reviewsPerRepository);
+
+
+            return result;
+        }
+
+        private string createPullRequestAndAssignUser(string p_organisationName, string p_repositoryName, string p_sha, string p_username)
+        {
+            string newBranchName = $"Feedback-{p_username}";
+
+            //un return a ete mis a chaque pour que si un erreur arrive entre les actions sa stop
+            string resultCreateBranch = this._githubApiAction.CreateNewBranchForFeedbackGitHub(p_organisationName, p_repositoryName, p_sha, newBranchName);
+            if (resultCreateBranch != "Created")
+                return "Branch not created";
+
+            string resultCreatePR = this._githubApiAction.CreateNewPullRequestFeedbackGitHub(p_organisationName, p_repositoryName, newBranchName, newBranchName.ToLower(), "Voici ou vous devez mettre vos commentaires");
+            if (resultCreatePR != "Created")
+                return "PullRequest not created";
+
+            string resultAddStudent = this._githubApiAction.AddStudentAsCollaboratorToPeerRepositoryGithub(p_organisationName, p_repositoryName, p_username);
+            if (resultAddStudent != "Created")
+                return "Student not added";
+
+            return "Created";
+        }
+
+
+        private List<Repository> getRepositoriesToAssign(string p_organisationName, string p_classRoomName, string p_assignmentName, List<Student> p_students)
+        {
+            List<Assignment> assignmentsResult = _depotClassroom.GetAssignmentsByClassroomName(p_classRoomName);
+
+            if (assignmentsResult.Count < 1)
+            {
+                throw new ArgumentOutOfRangeException("number of repository is insufficient");
+            }
+
+            Assignment assignment = assignmentsResult.SingleOrDefault(assignment => assignment.Name == p_assignmentName);
+
+            if (assignment == null)
+            {
+                throw new ArgumentNullException("assignment is null");
+            }
+
+            List<Repository> repositories = new List<Repository>();
+            List<Repository> repositoriesResult = this._depotRepository.GetRepositoriesFromOrganisationName(p_organisationName);
+
+            foreach (Repository repository in repositoriesResult)
+            {
+                string[] splitRepository = repository.Name.Split('-');
+
+                if (splitRepository[0] == assignment.Name)
+                {
+                    foreach (Student student in p_students)
                     {
-                        string[] branchName = branch.reference.Split("/");
-
-                        if (branchName[2] == "feedback")
+                        if (splitRepository[1] == student.Username)
                         {
-                            branchDTO = branch;
+                            repositories.Add(repository);
                             break;
                         }
                     }
 
-                    if (branchDTO != null)
-                    {
-                        int numberStudentAdded = 0;
-
-                        for (int i = 0; i < students.Count; i++)
-                        {
-                            if (students[i].Username.ToLower() != splitRepository[1].ToLower() && numberStudentAdded <= p_ReviewsPerRepository)
-                            {
-                                string newBranchName = $"Feedback-{students[i].Username}";
-
-                                string resultCreateBRanch = this._githubApiAction.CreateNewBranchForFeedbackGitHub(p_organisationName, repository.Name, branchDTO.gitObject.sha, newBranchName);
-
-                                string resultCreatePR = this._githubApiAction.CreateNewPullRequestFeedbackGitHub(p_organisationName, repository.Name, newBranchName, newBranchName.ToLower(), "Voici ou vous devez mettre vos commentaires");
-
-                                this._githubApiAction.AddStudentAsCollaboratorToPeerRepositoryGithub(p_organisationName, repository.Name, students[i].Username);
-                                numberStudentAdded++;
-                            }
-                        }
-
-                    }
-                }
-
-                return "ok";
-            }
-
-            return "not ok";
-        }
-
-        public List<Repository> getRepositoriesToAssign(string p_organisationName, string p_classRoomName, string p_assignmentName, List<Student> p_students)
-        {
-            List<Assignment> assignmentsResult = _depotClassroom.GetAssignmentsByClassroomName(p_classRoomName);
-
-            if (assignmentsResult.Count >= 1)
-            {
-                Assignment assignment = assignmentsResult.SingleOrDefault(assignment => assignment.Name == p_assignmentName);
-
-                if (assignment != null)
-                {
-                    List<Repository> repositories = new List<Repository>();
-                    List<Repository> repositoriesResult = this._depotRepository.GetRepositoriesFromOrganisationName(p_organisationName);
-
-                    foreach (Repository repository in repositoriesResult)
-                    {
-                        string[] splitRepository = repository.Name.Split('-');
-
-                        if (splitRepository[0] == assignment.Name)
-                        {
-                            foreach (Student student in p_students)
-                            {
-                                if (splitRepository[1] == student.Username)
-                                {
-                                    repositories.Add(repository);
-                                }
-                            }
-
-                        }
-                    }
-
-                    return repositories;
                 }
             }
 
-            throw new ArgumentOutOfRangeException("Not enough students");
+            return repositories;
         }
     }
 }
