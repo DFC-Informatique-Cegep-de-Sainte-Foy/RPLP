c4e2a291a37d70701513e4faa092396dff921b50
1996167 <melissa.lachapelle1@gmail.com>
2022-05-06 10:22:34 -0400

    Ajout d'une parenthèse

--- RPLP.API/Program.cs
+++ RPLP.API/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<RPLPDbContext>(
          options => options.UseSqlServer("Server=rplp.db; Database=RPLP; User Id=sa; password=Cad3pend86!"));
-         //options => options.UseSqlServer("Server=localhost,1433; Database=RPLP; User Id=sa; password=Cad3pend86!");
+         //options => options.UseSqlServer("Server=localhost,1433; Database=RPLP; User Id=sa; password=Cad3pend86!"));
 
 builder.Services.AddScoped<IDepotClassroom, DepotClassroom>();
 builder.Services.AddScoped<IDepotAdministrator, DepotAdministrator>();
