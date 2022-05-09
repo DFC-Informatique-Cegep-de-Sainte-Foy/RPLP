d26e9fdc1c84f1491bb61ddba2b6544193a2f219
1141200 <1141200@csfoy.ca>
2022-05-09 15:24:59 -0400

    Functionnal in dockercomposed context

--- RPLP.COHERENCECHECKER/Program.cs
+++ RPLP.COHERENCECHECKER/Program.cs
@@ -6,26 +6,12 @@ using System.Timers;
 string token = "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";
 ScriptGithubRPLP scripts = new ScriptGithubRPLP(new DepotClassroom(), new DepotRepository(), new DepotOrganisation(), token);
 
-scripts.EnsureOrganisationRepositoriesAreInDB();
-
-//System.Timers.Timer timer = new System.Timers.Timer();
-//timer.Elapsed += new ElapsedEventHandler(RunScriptEvent);
-//timer.Interval = 1000;
-//timer.AutoReset = true;
-//timer.Enabled = true;
-
-//while(true)
-//{
-//    Console.ReadLine();
-//}
-
-// // Specify what you want to happen when the Elapsed event is raised.
-//void RunScriptEvent(object source, ElapsedEventArgs e)
-//{
-//    scripts.EnsureOrganisationRepositoriesAreInDB();
-//    //Console.WriteLine("Interval");
-//    timer.Interval = 500000;
-//}
+while(true)
+{
+    scripts.EnsureOrganisationRepositoriesAreInDB();
+    Console.WriteLine("Interval");
+    Thread.Sleep(3600000);
+}
 
 
 
--- RPLP.DAL.SQL/Depots/DepotOrganisation.cs
+++ RPLP.DAL.SQL/Depots/DepotOrganisation.cs
@@ -16,8 +16,8 @@ namespace RPLP.DAL.SQL.Depots
 
         public DepotOrganisation()
         {
-            //this._context = new RPLPDbContext(new DbContextOptionsBuilder<RPLPDbContext>().UseSqlServer("Server=rplp.db; Database=RPLP; User Id=sa; password=Cad3pend86!").Options);
-            this._context = new RPLPDbContext(new DbContextOptionsBuilder<RPLPDbContext>().UseSqlServer("Server=localhost,1433; Database=RPLP; User Id=sa; password=Cad3pend86!").Options);
+            this._context = new RPLPDbContext(new DbContextOptionsBuilder<RPLPDbContext>().UseSqlServer("Server=rplp.db; Database=RPLP; User Id=sa; password=Cad3pend86!").Options);
+            //this._context = new RPLPDbContext(new DbContextOptionsBuilder<RPLPDbContext>().UseSqlServer("Server=localhost,1433; Database=RPLP; User Id=sa; password=Cad3pend86!").Options);
         }
 
         public DepotOrganisation(RPLPDbContext p_context)
