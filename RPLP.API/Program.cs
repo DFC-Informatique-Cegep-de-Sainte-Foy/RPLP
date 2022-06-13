785696afa68985e463cc7ee03eef47a5334cf613
Pierre-Fran�ois L�on <pfleon@cegep-ste-foy.qc.ca>
2022-06-13 18:31:13 +0000

    Updated Program.cs

--- RPLP.API/Program.cs
+++ RPLP.API/Program.cs
@@ -31,11 +31,11 @@ builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseAuthentication();
 app.UseAuthorization();
