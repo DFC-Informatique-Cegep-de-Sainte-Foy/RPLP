7824307db3d1a9af96a931993c806b8a5a72c7d3
Pierre-Fran�ois L�on <pfleon@cegep-ste-foy.qc.ca>
2022-06-13 11:53:23 +0000

    Updated Program.cs

--- RPLP.COHERENCECHECKER/Program.cs
+++ RPLP.COHERENCECHECKER/Program.cs
@@ -3,7 +3,7 @@ using RPLP.DAL.SQL.Depots;
 using RPLP.SERVICES.Github;
 using System.Timers;
 
-string token = "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";
+string token = "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";
 ScriptGithubRPLP scripts = new ScriptGithubRPLP(new DepotClassroom(), new DepotRepository(), new DepotOrganisation(), token);
 
 while(true)
