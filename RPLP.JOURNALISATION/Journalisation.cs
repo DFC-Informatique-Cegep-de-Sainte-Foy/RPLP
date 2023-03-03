746453db62eb93e869f5bbcf0b47fbafa459d348
casio g <casio.goncalves@hotmail.com>
2023-03-03 16:19:33 -0500

    ajout file log

--- RPLP.JOURNALISATION/Journalisation.cs
+++ RPLP.JOURNALISATION/Journalisation.cs
@@ -9,40 +9,37 @@ namespace RPLP.JOURNALISATION
     public static class Journalisation
     {
         //A changer pour la bd
-        private static string CheminDossierLogs = @"/app/log";
-        private static string CheminFichierDeLogs = CheminDossierLogs + "/" + "Log_Revue_Par_Les_Paires.csv";
+        //private static string CheminDossierLogs = @"/app/logs";
+        //private static string CheminFichierDeLogs = CheminDossierLogs + "/Log_Revue_Par_Les_Paires.csv";
 
         public static void Journaliser(Log log)
         {
-            CreerDossierSiNonExistant();
+            //CreerDossierSiNonExistant();
             AjouterJournalisation(log);
         }
 
-        private static void CreerDossierSiNonExistant()
-        {
-            if (!Directory.Exists(CheminDossierLogs))
-            {
-                Directory.CreateDirectory(CheminDossierLogs);
-            }
+        // private static void CreerDossierSiNonExistant()
+        // {
+        //     if (!Directory.Exists(CheminDossierLogs))
+        //     {
+        //         Directory.CreateDirectory(CheminDossierLogs);
+        //     }
+        //
+        //     CreerNomDuFichierLog();
+        // }
 
-            CreerNomDuFichierLog();
-        }
-
-        private static void CreerNomDuFichierLog()
-        {
-            if (!File.Exists(CheminFichierDeLogs))
-            {
-                File.Create(CheminFichierDeLogs);
-            }
-        }
+        // private static void CreerNomDuFichierLog()
+        // {
+        //     if (!File.Exists(CheminFichierDeLogs))
+        //     {
+        //         var fichierLog = File.Create(CheminFichierDeLogs);
+        //         fichierLog.Close();
+        //     }
+        // }
 
         private static void AjouterJournalisation(Log log)
         {
-            using (StreamWriter sw = new StreamWriter(CheminFichierDeLogs))
-            {
-                sw.WriteLine(log.ToString());
-                //File.AppendAllLines(CheminFichierDeLogs, new List<string> { log.ToString() });
-            }
+            File.AppendAllLines(@"logs/Log_Revue_Par_Les_Paires.csv", new List<string> { log.ToString() });
         }
     }
 }
