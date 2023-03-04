6bb14696233d5838911bf0019289a54f0198a574
Alexandre Dussault <2195759@csfoy.ca>
2023-03-03 20:03:43 -0500

    Ajout Fil de message

--- RPLP.JOURNALISATION/Program.cs
+++ RPLP.JOURNALISATION/Program.cs
@@ -1,8 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using RPLP.JOURNALISATION;
+
+Thread.Sleep(5000);
+
+ConsommateurJournalisation consommateur = new ConsommateurJournalisation();
+consommateur.DeclarerLaQueue();
 
 while (true)
 {
-    Console.WriteLine("start journalisation");
-    Thread.Sleep(3600000);
+    consommateur.Ecouter();
 }
