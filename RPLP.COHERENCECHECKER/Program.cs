1e2517c9b23b875d8d88fdcf0535680e6a9e9365
DylanBolduc-Veilleux <dylanbolducveilleux@gmail.com>
2023-03-27 12:02:39 -0400

    Debug container Unity

--- RPLP.COHERENCECHECKER/Program.cs
+++ RPLP.COHERENCECHECKER/Program.cs
@@ -1,16 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using RPLP.DAL.SQL;
 using RPLP.DAL.SQL.Depots;
 using RPLP.SERVICES.Github;
 using RPLP.SERVICES.InterfacesDepots;
+using System.Data;
 using System.Timers;
 using Unity;
+using Unity.Builder;
 using Unity.Injection;
 
+// à supprimer après test
+//IUnityContainer container = new UnityContainer().AddExtension(new Diagnostic());
+//IConfiguration configuration = container.Resolve<IConfiguration>(); 
+
+IConfigurationRoot configuration;
 IUnityContainer container = new UnityContainer();
-IConfiguration configuration = container.Resolve<IConfiguration>();
+
+var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+configuration = builder.Build();
+
 var connectionString = configuration.GetConnectionString("DefaultConnection");
 var token = configuration.GetSection("Token");
 
@@ -23,6 +36,7 @@ container.RegisterType<RPLPDbContext>(TypeLifetime.Singleton, new InjectionConst
 container.RegisterType<IDepotClassroom, DepotClassroom>(TypeLifetime.Scoped);
 container.RegisterType<IDepotOrganisation, DepotOrganisation>(TypeLifetime.Scoped);
 container.RegisterType<IDepotRepository, DepotRepository>(TypeLifetime.Scoped);
+
 
 ScriptGithubRPLP scripts = new ScriptGithubRPLP(container.Resolve<IDepotClassroom>(), container.Resolve<IDepotRepository>(), container.Resolve<IDepotOrganisation>(), token.ToString());
 
