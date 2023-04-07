f6bde54fec2fdacf3af0ea8e8d08387402cd6b7d
Alexandre Dussault <2195759@csfoy.ca>
2023-04-06 21:05:56 -0400

    Considionnemnt par rapport au status recu et message pour assignation des élèves das le but futur de gérer les professeurs.

--- RPLP.VALIDATIONS/Consummer.cs
+++ RPLP.VALIDATIONS/Consummer.cs
@@ -23,7 +23,7 @@ namespace RPLP.VALIDATIONS
         private ScriptGithubRPLP Script;
         private Allocations_JSONDTO allocations;
 
-        private string[] subject = { "rplp.assignations.students" };
+        private string[] subject = { "rplp.assignations.students", "rplp.assignations.professor" };
         public Consummer(ScriptGithubRPLP script)
         {
             this.Script = script;
@@ -44,7 +44,7 @@ namespace RPLP.VALIDATIONS
                      );
 
                     canalDeCommunication.QueueDeclare(
-                    "rplp-message-thread-assignation-students",
+                    "rplp-message-thread-assignation",
                     durable: false,
                     exclusive: false,
                     autoDelete: false,
@@ -56,7 +56,6 @@ namespace RPLP.VALIDATIONS
 
         public void Listen()
         {
-
             using (IConnection connexion = ConnexionFactory.CreateConnection())
             {
                 using (IModel canalDeCommunication = connexion.CreateModel())
@@ -64,7 +63,7 @@ namespace RPLP.VALIDATIONS
                     foreach (string sujet in subject)
                     {
                         canalDeCommunication.QueueBind(
-                            queue: "rplp-message-thread-assignation-students",
+                            queue: "rplp-message-thread-assignation",
                             exchange: "rplp-message-thread",
                             routingKey: sujet);
                     }
@@ -77,45 +76,72 @@ namespace RPLP.VALIDATIONS
                         string messageNonConvertit = Encoding.UTF8.GetString(donnees);
                         string sujet = argumentEvenement.RoutingKey;
 
-                        MessageGitHubAPI message = JsonConvert.DeserializeObject<MessageGitHubAPI>(messageNonConvertit);
-
-                        this.allocations = message.Allocations;
-
-                        foreach (Allocation allocation in this.allocations.Pairs)
+                        if(sujet == "rplp.assignations.students")
                         {
-                            string p_organisationName = message.Allocations._classroom.OrganisationName;
+                            MessageGitHubAPI message = JsonConvert.DeserializeObject<MessageGitHubAPI>(messageNonConvertit);
 
-                            string p_repositoryName = this.Script.getNameOfRepository(allocation.RepositoryId);
-
-                            string p_reviewerName = message.Allocations._classroom.Students.FirstOrDefault(reviewer => reviewer.Id == allocation.StudentId).Username;
-
-                            if (allocation.Status == 2)
-                            {
-                                RPLP.JOURNALISATION.Logging.Instance.Journal(new Log($"Assignation effectuée au 10 secondes :" +
-                                                $" organisationName : {p_organisationName} - repositoryName : {p_repositoryName} - NomDuReviewer : {p_reviewerName}"));
-                                Thread.Sleep(10000);
-                                this.Script.createPullRequestAndAssignUser(p_organisationName, p_repositoryName, p_reviewerName);
-                                this.Script.SetAllocationAfterAssignation(allocation);
-                            }
-                        }
-
-                        this.allocations.Pairs = this.Script.GetAllocationBySelectedAllocationID(message.Allocations.Pairs);
-
-                        if (this.allocations.Status == 3)
-                        {
-                            RPLP.JOURNALISATION.Logging.Instance.Journal(new Log($"Assignations complétées avec succès - Message ID : {message.MessageID}"));
-
-                            canalDeCommunication.BasicAck(argumentEvenement.DeliveryTag, false);
+                            ManageAssignmentStudent(message, canalDeCommunication, argumentEvenement);
                         }
                     };
 
-                    canalDeCommunication.BasicConsume(queue: "rplp-message-thread-assignation-students",
+                    canalDeCommunication.BasicConsume(queue: "rplp-message-thread-assignation",
                                         autoAck: false,
-                                        consumerTag: "rplp-message-thread-assignation-students",
+                                        consumerTag: "rplp-message-thread-assignation",
                                         consumer: consommateurServeur);
 
                     faireAttendreProgrammePrincipal.WaitOne();
                 }
+            }
+        }
+
+        public void ManageAssignmentStudent(MessageGitHubAPI message, IModel canalDeCommunication, BasicDeliverEventArgs argumentEvenement)
+        {
+            this.allocations = message.Allocations;
+
+            string status = "Stand";
+
+            foreach (Allocation allocation in this.allocations.Pairs)
+            {
+                string p_organisationName = message.Allocations._classroom.OrganisationName;
+
+                string p_repositoryName = this.Script.getNameOfRepository(allocation.RepositoryId);
+
+                string p_reviewerName = message.Allocations._classroom.Students.FirstOrDefault(reviewer => reviewer.Id == allocation.StudentId).Username;
+
+                if (allocation.Status == 2)
+                {
+                    RPLP.JOURNALISATION.Logging.Instance.Journal(new Log($"Assignation effectuée au 10 secondes :" +
+                                    $" organisationName : {p_organisationName} - repositoryName : {p_repositoryName} - NomDuReviewer : {p_reviewerName}"));
+                    
+                    Thread.Sleep(10000);
+
+                    status = this.Script.createPullRequestAndAssignUser(p_organisationName, p_repositoryName, p_reviewerName);
+
+                    if (status == "Forbidden")
+                    {
+                        RPLP.JOURNALISATION.Logging.Instance.Journal(new Log($"Assignation arrêté - status Forbidden reçu"));
+
+                        Thread.Sleep(60000);
+                    }
+                    else
+                    {
+                        if (status == "Created")
+                        {
+                            RPLP.JOURNALISATION.Logging.Instance.Journal(new Log($"Allocation : {allocation.Id} est affecté au status 3"));
+
+                            this.Script.SetAllocationAfterAssignation(allocation);
+                        }
+                    }
+                }
+            }
+
+            this.allocations.Pairs = this.Script.GetAllocationBySelectedAllocationID(message.Allocations.Pairs);
+
+            if (this.allocations.Status == 3)
+            {
+                RPLP.JOURNALISATION.Logging.Instance.Journal(new Log($"Assignations complétées avec succès - Message ID : {message.MessageID}"));
+
+                canalDeCommunication.BasicAck(argumentEvenement.DeliveryTag, false);
             }
         }
     }
